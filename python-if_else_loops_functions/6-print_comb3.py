a7161e9c447a9ac042afc17121a9da5a4eea6714
reha96 <reha.tuncer@gmail.com>
2026-04-25 14:31:07 +0000

    fix comma at end

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,7 @@ for i in range(10):
     for j in range(1,10):
         if i == j:
             continue
-        elif i < j:
+        elif i < j and i != 8:
             print("{}".format(i) + "{}".format(j) + ", ", end="")
+        elif i < j and i == 8:
+            print("{}".format(i) + "{}".format(j))
