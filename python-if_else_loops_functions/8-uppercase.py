bf5349b8b396da1c204a658b323896eabe4768de
reha96 <reha.tuncer@gmail.com>
2026-04-25 15:24:10 +0000

    use 2 print() only

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,6 @@ def uppercase(str):
     for i in range(len(input)):
         x = ord(input[i])
         if x >= 97 and x <= 123:
-            print("{}".format(chr(x-32)), end="")
-        else:
-            print("{}".format(chr(x)), end="")
+            x -= 32
+        print("{}".format(chr(x)), end="")
     print("")
