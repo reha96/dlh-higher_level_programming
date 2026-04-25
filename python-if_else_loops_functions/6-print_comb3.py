eb261bbba7562ff6ffe1989121102dd96a11ee1f
reha96 <reha.tuncer@gmail.com>
2026-04-25 14:26:38 +0000

    update 6 syntax

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ for i in range(10):
     for j in range(1,10):
         if i == j:
             continue
-        elif i<j:
+        elif i < j:
             print("{}".format(i) + "{}".format(j) + ", ", end="")
