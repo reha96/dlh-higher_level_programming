42cae8b9d185e0441bb72c194066de9627fa9ed2
reha96 <reha.tuncer@gmail.com>
2026-04-25 18:56:36 +0000

    vBatu

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    string = str(number)[-1]
-    digit = int(string)
-    print("{}".format(digit), end="")
+    # string = str(number)[-1]
+    # digit = int(string)
+    # print("{}".format(digit), end="")
+    print("{}".format(number % 10), end="")
     
