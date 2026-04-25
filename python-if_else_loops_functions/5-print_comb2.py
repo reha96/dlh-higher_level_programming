1f0a9961e2fc56d95528e9fc18d5521e1ba185da
reha96 <reha.tuncer@gmail.com>
2026-04-25 14:15:07 +0000

    fix 98

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(99):
-    if i < 98:
+for i in range(98):
+    if i < 97:
         print("{:02d}".format(i)+", ", end="")
     else:
         print("{}".format(i))
