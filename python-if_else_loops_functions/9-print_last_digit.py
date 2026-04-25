6cef7d3aeea985a410d0a47594fcacfce032b5ad
reha96 <reha.tuncer@gmail.com>
2026-04-25 19:05:41 +0000

    fix 9 return and print

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    # string = str(number)[-1]
-    # digit = int(string)
-    # print("{}".format(digit), end="")
-    print("{}".format(number % 10), end="")
-    
+    string = str(number)[-1]
+    digit = int(string)
+    print("{}".format(digit), end="")
+return digit
