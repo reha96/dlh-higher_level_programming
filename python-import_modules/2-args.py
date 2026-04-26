3922fbd4a70a944f5d1da1e37008bb8f6300aa06
reha96 <reha.tuncer@gmail.com>
2026-04-26 18:48:31 +0000

    create 2-

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
+import sys
 
-
-def main(argv):
-    if argv == None:
+def main(*argv):
+    if len(argv) == 0:
         print("0 arguments.")
     elif len(argv) == 1:
         print("1 argument:")
-        print(f"{len(argv)}: {argv}")     
+        print(f"{len(argv)}: {argv[0]}")     
     else:
         print(f"{len(argv)} arguments:")
         for i in range(len(argv)):
-            print(f"{i}: {argv[i]}")
+            print(f"{i+1}: {argv[i]}")
 
 if __name__ == "__main__":
-    main()
+    main(*sys.argv[1:])
