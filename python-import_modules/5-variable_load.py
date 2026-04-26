9537d7e221e9c878ce8ddebf53d4218be316e9ec
reha96 <reha.tuncer@gmail.com>
2026-04-26 20:03:43 +0000

    create 5-

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-import variable_load_5
+from variable_load_5 import a
 
 
 def main():
-    for x in variable_load_5:
-        if dir(variable_load_5[x]) == "a": 
-            print("{}".format(variable_load_5[x]))
+    print(a)
 
 
 if __name__ == "__main__":
