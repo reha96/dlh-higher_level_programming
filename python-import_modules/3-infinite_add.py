6f3206a2124a6ec4efaa4425f52ef2ae623efa97
reha96 <reha.tuncer@gmail.com>
2026-04-26 19:20:54 +0000

    create 3-

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/bin/usr/python3
+import sys
+
+
+def main(*argv):
+    sum = 0
+    for i in range(len(argv)):
+        sum += int(argv[i])
+    print(f"{sum}")
+
+
+if __name__ == "__main__":
+    main(*sys.argv[1:])
