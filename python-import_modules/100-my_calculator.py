db8717a0e7aa90e9e5dfb1cc8ccd9cfb56b61e16
reha96 <reha.tuncer@gmail.com>
2026-04-26 20:27:34 +0000

    create 100

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from calculator_1.py import add, sub, mul, div
+
+
+def main(a, operator, b):
+    if a is None or operator is None or b is None:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    a = int(a)
+    b = int(b)
+    if operator == "+":
+        print("{}".format(a) + "{}".format(operator) + "{} = ".format(b) + "{}".format(add(a, b)))
+    elif operator == "-":
+        print("{}".format(a) + "{}".format(operator) + "{} = ".format(b) + "{}".format(sub(a, b)))
+    elif operator == "*":
+        print("{}".format(a) + "{}".format(operator) + "{} = ".format(b) + "{}".format(mul(a, b)))
+    elif operator == "/":
+        print("{}".format(a) + "{}".format(operator) + "{} = ".format(b) + "{}".format(div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+
+
+if __name__ == "__main__":
+    main()
