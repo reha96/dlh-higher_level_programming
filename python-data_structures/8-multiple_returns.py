af46738132ff29e6d1786ee97555ca2312517d30
reha96 <51120957+reha96@users.noreply.github.com>
2026-04-28 21:14:35 +0200

    fix "" case

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first = sentence[0]
-    return (length, first)
+    if length == 0:
+        return None
+    else:
+        first = sentence[0]
+        return (length, first)
