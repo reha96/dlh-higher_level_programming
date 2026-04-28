0956b75b1ce2dffb39f24229787c252a70b449c5
reha96 <51120957+reha96@users.noreply.github.com>
2026-04-28 13:27:03 +0200

    fix end of line empty space

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,8 +4,7 @@ def print_matrix_integer(matrix=[[]]):
         if i>0:
             print(f"\n", end="")
         for j in range(len(matrix[i])):
-            if j < len(matrix[i]):
-                print("{:d} ".format(matrix[i][j]), end="")
-            else:
-                print("{:d}".format(matrix[i][j]), end="")
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != len(matrix[i])-1:
+                print(" ", end="")
     print(f"\n", end="")
