80c9ba5308d8c71c74b854057a87c663ffa3f7a5
reha96 <51120957+reha96@users.noreply.github.com>
2026-04-28 09:08:08 +0200

    fix 2

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -3,5 +3,5 @@ def replace_in_list(my_list, idx, element):
     if idx >= len(my_list) or idx < 0:
         return my_list
     else:
-        my_list[idx-1] = element
+        my_list[idx] = element
         return my_list[idx]
