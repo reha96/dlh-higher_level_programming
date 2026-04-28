affdfc1cf0e98681802cb863220d574df2f5b9c5
reha96 <51120957+reha96@users.noreply.github.com>
2026-04-28 13:27:31 +0200

    whitespace operator fix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -5,6 +5,6 @@ def print_matrix_integer(matrix=[[]]):
             print(f"\n", end="")
         for j in range(len(matrix[i])):
             print("{:d}".format(matrix[i][j]), end="")
-            if j != len(matrix[i])-1:
+            if j != len(matrix[i]) - 1:
                 print(" ", end="")
     print(f"\n", end="")
