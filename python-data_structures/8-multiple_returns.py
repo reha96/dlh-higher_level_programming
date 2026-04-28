7aba079e686dc1d67b6348b66aaed7674046a54c
reha96 <51120957+reha96@users.noreply.github.com>
2026-04-28 21:16:59 +0200

    fix 3 empty string

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     length = len(sentence)
     first = sentence[0]
-    if length == 0:
-        return (length, None)
-    else:
+    if length > 0:
         return (length, first)
+    else:
+        return (0, None)
