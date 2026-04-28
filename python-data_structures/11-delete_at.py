7e3b1fd1e8a3d8eb938293775d4ee8b410daf7df
reha96 <51120957+reha96@users.noreply.github.com>
2026-04-28 21:51:41 +0200

    fix using list methods

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    out = []
     if idx < 0 or idx > len(my_list):
         return my_list
     else:
-        for i in range(len(my_list)):
-            if i != idx:
-                out.append(my_list[i])
-        return out
+        my_list.remove(idx)
+    return my_list
