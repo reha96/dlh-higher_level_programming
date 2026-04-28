2bb5bcffdf8b8587bca17092bcf9e12ccdc44ef2
reha96 <51120957+reha96@users.noreply.github.com>
2026-04-28 12:42:54 +0200

    fix space

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,8 +3,8 @@ def no_c(my_string):
     for i in range(len(my_string)):
         if "C" in my_string:
             my_string = my_string.split('C')
-            my_string = ' '.join(my_string)
+            my_string = ''.join(my_string)
         if "c" in my_string:
             my_string = my_string.split('c')
-            my_string = ' '.join(my_string)
+            my_string = ''.join(my_string)
     return my_string
