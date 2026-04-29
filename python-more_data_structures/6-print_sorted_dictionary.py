c3221a63e25f87e445a770e7f35c78aef85b6d03
reha96 <51120957+reha96@users.noreply.github.com>
2026-04-29 14:27:29 +0200

    Update 6-print_sorted_dictionary.py

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,7 +2,5 @@
 def print_sorted_dictionary(a_dictionary):
     keys = list(a_dictionary.keys())
     keys.sort()
-    print(keys)
-
-a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
-print_sorted_dictionary(a_dictionary)
+    for i in keys:
+        print("{} : ".format(i) + "{}".format(a_dictionary.get(i)))
