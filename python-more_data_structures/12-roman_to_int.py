eec9deb78b8c58d96d3d03c985d95475d0545548
reha96 <51120957+reha96@users.noreply.github.com>
2026-04-30 17:37:58 +0200

    Update 12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     out = 0
-    if type(roman_string) is not str or type(roman_string) is None:
+    if (type(roman_string) is not str) or (type(roman_string) is None):
         return None
     map = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
     i = 0
@@ -9,6 +9,7 @@ def roman_to_int(roman_string):
     excep = False
     if len(roman_string) == 1:
         out += map[roman_string[i]]
+        return out
     # 1 length case, no comparison
     for i in range(len(roman_string)):
         if excep == True:
