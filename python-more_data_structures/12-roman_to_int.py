3d38fd3c1299f4d22ef0af4604b6200dd27b9d65
reha96 <51120957+reha96@users.noreply.github.com>
2026-04-30 14:37:37 +0200

    Create 12-roman_to_int.py

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+    out = 0
+    for i in range(len(roman_string)):
+        if roman_string[i] == "M" and roman_string[i-1] == "C":
+            out += 900
+        elif roman_string[i] == "M":
+            out += 1000
+        if roman_string[i] == "D" and roman_string[i-1] == "C":
+            out += 400
+        elif roman_string[i] == "D":
+            out += 500
+        elif roman_string[i] == "C":
+            out += 100
+        if roman_string[i] == "L" and roman_string[i-1] == "X":
+            out += 40
+        elif roman_string[i] == "L":
+            out += 50
+        if (roman_string[i] == "X" and (i == 0)) or (roman_string[i] == "X" and roman_string[i-1] != "I"):
+            out += 10
+        if roman_string[i] == "X" and roman_string[i-1] == "I":
+            out += 9
+        if roman_string[i] == "I" and roman_string[i-1] != "X":
+            out += 1
+        if (roman_string[i] == "V" and (i == 0)) or (roman_string[i] == "V" and roman_string[i-1] != "I"):
+            out += 5
+        elif roman_string[i] == "V" and roman_string[i-1] == "I":
+            out += 4
+    return out
