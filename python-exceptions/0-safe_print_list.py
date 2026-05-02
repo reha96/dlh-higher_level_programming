a953b691ce91e14f28ac2bb04edad40f719fd2cc
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-02 10:29:04 +0200

    Create 0-safe_print_list.py

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    out_1 = 0
+    for x in range(x):
+        try:
+            print(my_list[x], end="")
+            out_1 = x+1
+        except:
+            return out_1
+    print("")
+    return out_1
