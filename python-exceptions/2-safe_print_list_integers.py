9524ba875e5f875f0c17962285fbe318ecdb763d
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-02 12:09:34 +0200

    Update 2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     skipped = 0
-    try:
-        for i in range(x):
-            try:
+
+    for i in range(x):
+        try:
+            if type(my_list[i]) is int:
                 print("{:d}".format(my_list[i]), end="")
-            except:
-                print(i)
-                skipped += 1
-    except:
-        pass
-    finally:
-        print("")
-        return i-skipped+1
+        except TypeError:
+            skipped += 1
+    print("")
+    return i-skipped+1
