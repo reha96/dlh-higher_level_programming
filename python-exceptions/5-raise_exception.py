ea6bee2b91498f9b86c0075e5df9fd60a3d4240a
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-02 17:16:17 +0200

    Update 5-raise_exception.py

--- python-exceptions/5-raise_exception.py
+++ python-exceptions/5-raise_exception.py
@@ -1,9 +1,6 @@
 # /usr/bin/python3
 def raise_exception():
-    raise TypeError
-
-
-try:
-    raise_exception()
-except TypeError as te:
-    print("Exception raised")
+    try:
+        raise TypeError
+    except TypeError:
+        raise
