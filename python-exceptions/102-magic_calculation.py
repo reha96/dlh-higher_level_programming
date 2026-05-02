8b2ad3b75e388989ff57941fa27a23892e867b83
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-02 23:18:45 +0200

    Create 102-magic_calculation.py

---
+++ python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 4):
+        try:
+            if i > a:
+                raise ValueError("Too far")
+            else:
+                result += (a ** b) / i
+        except ValueError:
+            continue
+    result += a + b
+    return result
