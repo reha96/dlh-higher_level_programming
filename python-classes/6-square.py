3d9fb181cb0d7179bfa909773919b123ebe97d0e
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-05 17:14:46 +0200

    Create 6-square.py

---
+++ python-classes/6-square.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+"""This module creates a class that defines a square without
+importing any modules."""
+
+
+class Square:
+    """Create a class called Square that defines a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes the size of the Square, as a private
+        attribute.
+        1) size must be an integer, otherwise raise a TypeError
+        exception with the message size must be an integer.
+        2) if size is less than 0, raise a ValueError exception
+        with the message size must be >= 0
+
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+        if (type(position) is not tuple) or (position[0] < 0) or (position[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = position
+
+    @property
+    def position(self):
+        """Public instance method def position(self) returns the
+        current position."""
+        return self.__position
+
+    @position.setter
+    def position(self, position):
+        if type(position) is not int:
+            raise TypeError("size must be an integer")
+        elif position < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = position
+
+    def area(self):
+        """Public instance method def area(self) returns the
+        current square area."""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def my_print(self):
+        if self.size == 0:
+            print("")
+        for i in range(self.size):
+            for k in range(self.position[0]):
+                print("_", end="")
+            for j in range(self.size):
+                print("#", end="")
+            print("")
+
+
+# position should be set using space - Don't fill lines by spaces when position[1] > 0
+my_square_1 = Square(3)
+my_square_1.my_print()
+
+print("--")
+
+my_square_2 = Square(3, (1, 1))
+my_square_2.my_print()
+
+print("--")
+
+my_square_3 = Square(3, (3, 0))
+my_square_3.my_print()
+
+print("--")
