1c170a3039fd4044bfa1dc89b22ce73f6e4c5518
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-05 13:38:06 +0200

    fix 0 and create gitignore for pychache tests

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""This module creates a class that defines a square without importing any modules."""
+
+
 class Square:
     """Create an empty class Square that defines a square."""
     # An empty block
