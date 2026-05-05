9104023e4a641d247ea15028de93cd2b3bfb8b0f
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-05 15:16:44 +0200

    Update 3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -15,11 +15,11 @@ class Square:
         with the message size must be >= 0
 
         """
-        self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
 
     def area(self):
         """Public instance method def area(self) returns the
