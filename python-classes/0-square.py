bbba45bc50da9c5154ae097abe8462f50e99865f
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-05 13:40:54 +0200

    Update 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""This module creates a class that defines 
-a square without importing any modules."""
+"""This module creates a class that defines a square without importing any modules."""
 
 
 class Square:
-    """Create an empty class Square that 
-    defines a square."""
+    """Create an empty class Square that defines a square."""
     pass
