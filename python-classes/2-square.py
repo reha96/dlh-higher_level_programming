ffb41af2b15d1119664b077b017e30cc654ddf32
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-05 14:01:57 +0200

    Update 2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,36 +1,19 @@
 #!/usr/bin/python3
+"""This module creates a class that defines a square without importing any modules."""
 
 
+class Square:
+    """Create a class called Square that defines a square."""
 
+    def __init__(self, size=0):
+        """Initializes the size of the Square, as a private attribute. 
+        1) size must be an integer, otherwise raise a TypeError exception with the message size must be an integer. 
+        2) if size is less than 0, raise a ValueError exception with the message size must be >= 0
 
-# my_square_1 = Square(3)
-# print(type(my_square_1))
-# print(my_square_1.__dict__)
-
-# my_square_2 = Square()
-# print(type(my_square_2))
-# print(my_square_2.__dict__)
-
-# try:
-#     print(my_square_1.size)
-# except Exception as e:
-#     print(e)
-
-# try:
-#     print(my_square_1.__size)
-# except Exception as e:
-#     print(e)
-
-# try:
-#     my_square_3 = Square("3")
-#     print(type(my_square_3))
-#     print(my_square_3.__dict__)
-# except Exception as e:
-#     print(e)
-
-# try:
-#     my_square_4 = Square(-89)
-#     print(type(my_square_4))
-#     print(my_square_4.__dict__)
-# except Exception as e:
-#     print(e)
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
