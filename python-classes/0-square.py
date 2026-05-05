7f7587d492830a45d19f8f840c983c0041952e81
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-05 12:55:25 +0200

    create folder, 0 and 1

---
+++ python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    """Create an empty class Square that defines a square."""
+    pass  # An empty block
+
+
+my_square = Square()
+print(type(my_square))
+print(my_square.__dict__)
