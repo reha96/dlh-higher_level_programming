672be348d2c2a9a0706e32e4bdb413e154315d58
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-06 11:15:45 +0200

    Update 6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -15,19 +15,8 @@ class Square:
         with the message size must be >= 0
 
         """
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-
-        if type(position) is not tuple:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif (position[0] < 0) or (position[1] < 0):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def position(self):
