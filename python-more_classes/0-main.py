202afd3dbba5c47cfb33cf70b7b872aa165dfeb3
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-07 11:30:41 +0200

    create 0- and tester

---
+++ python-more_classes/0-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""tester file"""
+
+"""tester T1"""
+Rectangle = __import__('0-rectangle').Rectangle
+
+my_rectangle = Rectangle()
+print(type(my_rectangle))
+print(my_rectangle.__dict__)
