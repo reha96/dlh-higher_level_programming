ee4d33eb02612cc25df0f0562706c793102c08cd
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-07 11:21:07 +0200

    working on extra task p1 t1

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -13,8 +13,7 @@ class Node:
     def data(self, data):
         if not isinstance(data, int):
             raise TypeError("data must be an integer")
-        else:
-            self.__data = data
+        self.__data = data
 
     @property
     def next_node(self):
@@ -23,36 +22,72 @@ class Node:
     @next_node.setter
     def next_node(self, value):
         if (
-            not isinstance(value, Node) or
+            not isinstance(value, Node) and
             value is not None
         ):
             raise TypeError("next_node must be a Node object")
-        else:
-            self.__next_node = value
+        self.__next_node = value
 
 
 class SinglyLinkedList:
-
-    out = []
-
     def __init__(self):
-        pass
+        self.__head = None
 
     def sorted_insert(self, value):
-        self.out.append(value)
-        self.out.sort()
+        new_node = Node(value)
+        print(f"new_node.data {new_node.data}")
+        
+        if self.__head is None:
+            self.__head = new_node
+            return
+        
+        if new_node.data <= self.__head.data:
+            self.__head = new_node
+            print(f"self.__head.data {self.__head.data}")
+
+        
+        current = self.__head
+        prev = None
+        
+        # while current is not None and new_node.data >= current.data:
+        #     prev = current
+        #     current = current.next_node
+        #     prev.next_node = new_node
+        #     new_node.next_node = current
+        #     print(f"prev.data {prev.data}")
+            
+        
+        
+        # while current is not None:
+        #     prev = current
+        #     current = current.next_node
+        
+        # prev.next_node = new_node
+        # new_node.next_node = current
+
 
     def __str__(self):
-        for i in range(len(self.out)):
-            print(str(self.out[i]))
-    
+        current = self.__head
+        values = []
+        while current is not None:
+            values.append(str(current.data))  # Collect string representation of data
+            current = current.next_node  # Move to next node
+        return "\n".join(values)  # Join the collected data with new lines
 
-# And, write a class SinglyLinkedList that defines a singly linked list by:
-
-#     Private instance attribute: head (no setter or getter)
-#     Simple instantiation: def __init__(self):
-#     Should be printable:
-#     print the entire list in stdout
-#     one node number by line
-#     Public instance method: def sorted_insert(self, value): that inserts a new Node into the correct sorted position in the list (increasing order)
-#     You are not allowed to import any module
+sll = SinglyLinkedList()
+sll.sorted_insert(1)
+sll.sorted_insert(0)
+sll.sorted_insert(2)
+# sll.sorted_insert(5)
+# sll.sorted_insert(3)
+# sll.sorted_insert(3)
+# sll.sorted_insert(10)
+# sll.sorted_insert(1)
+# sll.sorted_insert(-4)
+# sll.sorted_insert(-3)
+# sll.sorted_insert(4)
+# sll.sorted_insert(5)
+# sll.sorted_insert(12)
+# sll.sorted_insert(3)
+# sll.sorted_insert(90)
+print(sll)
