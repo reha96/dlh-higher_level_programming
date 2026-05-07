a1b32cfe1fb5886b2a252fb07f67b0ff2ea4e400
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-07 13:26:32 +0200

    create 3 and update tester

--- python-more_classes/0-main.py
+++ python-more_classes/0-main.py
@@ -3,6 +3,9 @@
 """tester file"""
 
 """tester T1"""
+print("--")
+print("tester T1")
+print("--")
 Rectangle = __import__("0-rectangle").Rectangle
 
 my_rectangle = Rectangle()
@@ -10,6 +13,9 @@ print(type(my_rectangle))
 print(my_rectangle.__dict__)
 
 """tester T2"""
+print("--")
+print("tester T2")
+print("--")
 Rectangle = __import__("1-rectangle").Rectangle
 
 my_rectangle = Rectangle(2, 4)
@@ -33,6 +39,9 @@ except Exception as err:
     print("[{}] {}".format(err.__class__.__name__, err))
 
 """tester T3"""
+print("--")
+print("tester T3")
+print("--")
 Rectangle = __import__('2-rectangle').Rectangle
 
 my_rectangle = Rectangle(2, 4)
@@ -43,3 +52,39 @@ print("--")
 my_rectangle.width = 0
 my_rectangle.height = 3
 print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+"""tester T4"""
+print("--")
+print("tester T4")
+print("--")
+
+Rectangle = __import__('3-rectangle').Rectangle
+
+my_rectangle = Rectangle(2, 4)
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+print(str(my_rectangle))
+print(repr(my_rectangle))
+
+print("--")
+
+my_rectangle.width = 10
+my_rectangle.height = 3
+print(my_rectangle)
+print(repr(my_rectangle))
+
+try:
+    my_rectangle.width = 0
+    my_rectangle.height = 0
+    print(my_rectangle)
+    print(repr(my_rectangle))
+except Exception as err:
+    print("[{}] {}".format(err.__class__.__name__, err))
+
+try:
+    my_rectangle.width = -9
+    my_rectangle.height = 0
+    print(my_rectangle)
+    print(repr(my_rectangle))
+except Exception as err:
+    print("[{}] {}".format(err.__class__.__name__, err))
