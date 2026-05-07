1d684703d181125f5a56b37e617d80d079956b9b
reha96 <reha.tuncer@gmail.com>
2026-05-07 12:26:00 +0200

    update tester

--- python-more_classes/0-main.py
+++ python-more_classes/0-main.py
@@ -3,21 +3,21 @@
 """tester file"""
 
 """tester T1"""
-Rectangle = __import__('0-rectangle').Rectangle
+Rectangle = __import__("0-rectangle").Rectangle
 
 my_rectangle = Rectangle()
 print(type(my_rectangle))
 print(my_rectangle.__dict__)
 
-"""tester T2"""    
-Rectangle = __import__('1-rectangle').Rectangle
+"""tester T2"""
+Rectangle = __import__("1-rectangle").Rectangle
 
-# my_rectangle = Rectangle(2, 4)
-# print(my_rectangle.__dict__)
+my_rectangle = Rectangle(2, 4)
+print(my_rectangle.__dict__)
 
-# my_rectangle.width = 10
-# my_rectangle.height = 3
-# print(my_rectangle.__dict__)
+my_rectangle.width = 10
+my_rectangle.height = 3
+print(my_rectangle.__dict__)
 
 try:
     my_rectangle = Rectangle(-2, -4)
@@ -31,3 +31,5 @@ try:
     print(my_rectangle.__dict__)
 except Exception as err:
     print("[{}] {}".format(err.__class__.__name__, err))
+
+"""tester T3"""
