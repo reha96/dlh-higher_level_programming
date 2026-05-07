390ade4faf2d0aea5457ae31c91fd66b9c8c4e47
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-07 13:36:41 +0200

    Update 0-main.py

--- python-more_classes/0-main.py
+++ python-more_classes/0-main.py
@@ -91,3 +91,32 @@ except Exception as err:
 
 my_rectangle = Rectangle(2, 4)
 print(str(my_rectangle))
+
+"""tester T5"""
+print("--")
+print("tester T5")
+print("--")
+
+my_rectangle = Rectangle(2, 4)
+print(str(my_rectangle))
+print("--")
+print(my_rectangle)
+print("--")
+print(repr(my_rectangle))
+print("--")
+print(hex(id(my_rectangle)))
+print("--")
+
+# create new instance based on representation
+new_rectangle = eval(repr(my_rectangle))
+print(str(new_rectangle))
+print("--")
+print(new_rectangle)
+print("--")
+print(repr(new_rectangle))
+print("--")
+print(hex(id(new_rectangle)))
+print("--")
+
+print(new_rectangle is my_rectangle)
+print(type(new_rectangle) is type(my_rectangle))
