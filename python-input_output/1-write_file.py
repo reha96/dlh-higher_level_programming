edbc1f4adcdf4bbcbcd2b4e7658d6c87d763cdd3
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-08 12:38:32 +0200

    update tester and create 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module writes a function that writes a
+string to a text file (UTF8) and prints it
+to stdout"""
+
+
+def write_file(filename="", text=""):
+    try:
+        with open(filename)
+    except Exception:
+        
--- python-input_output/tests/0-main.py
+++ python-input_output/tests/0-main.py
@@ -11,15 +11,23 @@ print("--"*5 + "pycode" + "--"*5)
 
 read_file = __import__('0-read_file').read_file
 
-read_file("tests/my_file_0.txt")
+try:
+    read_file("tests/my_file_0.txt")
+except Exception as err:
+    print("[{}] {}".format(err.__class__.__name__, err))
 
 os.system('clear')
 print("--"*10)
 print("tester 0")
 print("--"*5 + "pycode" + "--"*5)
-os.system("pycodestyle 0-read_file.py")
+os.system("pycodestyle 1-write_file.py")
 print("--"*5 + "pycode" + "--"*5)
 
-read_file = __import__('0-read_file').read_file
+write_file = __import__('1-write_file').write_file
 
-read_file("tests/my_file_0.txt")
+try:
+    nb_characters = write_file(
+        "my_first_file.txt", "This School is so cool!\n")
+    print(nb_characters)
+except Exception as err:
+    print("[{}] {}".format(err.__class__.__name__, err))
