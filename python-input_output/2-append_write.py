6c3ec4cc22b82ad85b5b0ca5fe57f1c8f8414478
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-08 14:10:30 +0200

    test and push 2

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""A module that writes a function that
+appends a string at the end of a text file (UTF8)
+and returns the number of characters added
+    """
+
+
+def append_write(filename="", text=""):
+    """a function that
+    appends a string at the end of a text file
+    (UTF8) and returns the number of characters
+    added
+
+    Args:
+        filename (str, optional): _description_.
+        Defaults to "".
+        text (str, optional): _description_.
+        Defaults to "".
+    """
+    with open(filename, "a", encoding="utf-8") as file:
+        file.write(text)
+    with open(filename, "r", encoding="utf-8") as file:
+        return len(file.read())
--- python-input_output/tests/0-main.py
+++ python-input_output/tests/0-main.py
@@ -1,12 +1,11 @@
 """tester file"""
 import os
 
-task = 0
 os.system('clear')
 print("--"*10)
 print("tester 0")
 print("--"*5 + "pycode" + "--"*5)
-os.system(f"pycodestyle {task}-read_file.py")
+os.system("pycodestyle 0-read_file.py")
 print("--"*5 + "pycode" + "--"*5)
 
 read_file = __import__('0-read_file').read_file
@@ -16,12 +15,11 @@ try:
 except Exception as err:
     print("[{}] {}".format(err.__class__.__name__, err))
 
-task += 1
 os.system('clear')
 print("--"*10)
 print("tester 1")
 print("--"*5 + "pycode" + "--"*5)
-os.system(f"pycodestyle {task}-write_file.py")
+os.system("pycodestyle 1-write_file.py")
 print("--"*5 + "pycode" + "--"*5)
 
 write_file = __import__('1-write_file').write_file
@@ -33,15 +31,14 @@ try:
 except Exception as err:
     print("[{}] {}".format(err.__class__.__name__, err))
 
-task += 1
 os.system('clear')
 print("--"*10)
 print("tester 1")
 print("--"*5 + "pycode" + "--"*5)
-os.system(f"pycodestyle {task}-append_write.py")
+os.system(f"pycodestyle 2-append_write.py")
 print("--"*5 + "pycode" + "--"*5)
 
-append_write = __import__(f'{task}-append_write').write_file
+append_write = __import__('2-append_write').append_write
 
 try:
     nb_characters_added = append_write(
