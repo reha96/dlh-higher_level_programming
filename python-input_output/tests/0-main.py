5c94d37f27455d50d05c88aff7f4806f5f75ac78
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-08 12:30:02 +0200

    Update 0-main.py

--- python-input_output/tests/0-main.py
+++ python-input_output/tests/0-main.py
@@ -12,3 +12,14 @@ print("--"*5 + "pycode" + "--"*5)
 read_file = __import__('0-read_file').read_file
 
 read_file("tests/my_file_0.txt")
+
+os.system('clear')
+print("--"*10)
+print("tester 0")
+print("--"*5 + "pycode" + "--"*5)
+os.system("pycodestyle 0-read_file.py")
+print("--"*5 + "pycode" + "--"*5)
+
+read_file = __import__('0-read_file').read_file
+
+read_file("tests/my_file_0.txt")
