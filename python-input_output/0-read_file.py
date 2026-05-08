3924803c74165f02828004d5bf64e158cc7d7c5b
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-08 12:22:17 +0200

    create 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-"""This module writes a function that reads a text file (UTF8) and prints it to stdout"""
+"""This module writes a function that reads a
+text file (UTF8) and prints it to stdout"""
 
 
 def read_file(filename=""):
-    with open(filename, "r") as file:
-        data
+    try:
+        with open(filename, "r") as file:
+            print(file.read())
+    except Exception as err:
+        print(err)
--- python-input_output/tests/0-main.py
+++ python-input_output/tests/0-main.py
@@ -1,5 +1,14 @@
 """tester file"""
+import os
+
+
+os.system('clear')
+print("--"*10)
+print("tester 0")
+print("--"*5 + "pycode" + "--"*5)
+os.system("pycodestyle 0-read_file.py")
+print("--"*5 + "pycode" + "--"*5)
 
 read_file = __import__('0-read_file').read_file
 
-read_file("my_file_0.txt")
+read_file("tests/my_file_0.txt")
