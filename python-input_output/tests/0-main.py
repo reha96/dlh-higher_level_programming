ed4fda54cc5fd2084b94dffaa2b26bc1f6545008
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-08 13:46:15 +0200

    Update 0-main.py

--- python-input_output/tests/0-main.py
+++ python-input_output/tests/0-main.py
@@ -32,3 +32,20 @@ try:
     print(nb_characters)
 except Exception as err:
     print("[{}] {}".format(err.__class__.__name__, err))
+
+task += 1
+os.system('clear')
+print("--"*10)
+print("tester 1")
+print("--"*5 + "pycode" + "--"*5)
+os.system(f"pycodestyle {task}-write_file.py")
+print("--"*5 + "pycode" + "--"*5)
+
+write_file = __import__('1-write_file').write_file
+
+try:
+    nb_characters = write_file(
+        "my_first_file.txt", "This School is so cool!\n")
+    print(nb_characters)
+except Exception as err:
+    print("[{}] {}".format(err.__class__.__name__, err))
