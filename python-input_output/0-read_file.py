dc7111d3c39907d4f78021ff784885a8e119d80c
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-08 12:23:34 +0200

    create README and add comments

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,6 +4,9 @@ text file (UTF8) and prints it to stdout"""
 
 
 def read_file(filename=""):
+    """a function that reads a
+    text file (UTF8) and prints it to stdout"""
+
     try:
         with open(filename, "r") as file:
             print(file.read())
