ce3bc37cbb588eea76fce25c73d8deb008c59779
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-08 13:45:14 +0200

    Update 1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -8,10 +8,7 @@ def write_file(filename="", text=""):
     """a function that writes a
     string to a text file (UTF8) and returns the nb
     of chars"""
-    try:
-        with open(filename, "w", encoding="utf-8") as file:
-            file.write(text)
-        with open(filename, "r", encoding="utf-8") as file:
-            return len(file.read())
-    except Exception as err:
-        print("[{}] {}".format(err.__class__.__name__, err))
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(text)
+    with open(filename, "r", encoding="utf-8") as file:
+        return len(file.read())
