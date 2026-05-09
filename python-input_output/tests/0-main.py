7ce23619ac0b127e3edcd1694dcf23a1c65c8afb
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-09 09:41:04 +0200

    Update 0-main.py

--- python-input_output/tests/0-main.py
+++ python-input_output/tests/0-main.py
@@ -58,3 +58,39 @@ try:
     print(nb_characters_added)
 except Exception as err:
     print("[{}] {}".format(err.__class__.__name__, err))
+
+os.system('clear')
+print("--"*10)
+print("tester 1")
+print("--"*5 + "pycode" + "--"*5)
+os.system("pycodestyle 2-append_write.py")
+print("--"*5 + "pycode" + "--"*5)
+
+to_json_string = __import__('3-to_json_string').to_json_string
+
+my_list = [1, 2, 3]
+s_my_list = to_json_string(my_list)
+print(s_my_list)
+print(type(s_my_list))
+
+my_dict = { 
+    'id': 12,
+    'name': "John",
+    'places': [ "San Francisco", "Tokyo" ],
+    'is_active': True,
+    'info': {
+        'age': 36,
+        'average': 3.14
+    }
+}
+s_my_dict = to_json_string(my_dict)
+print(s_my_dict)
+print(type(s_my_dict))
+
+try:
+    my_set = { 132, 3 }
+    s_my_set = to_json_string(my_set)
+    print(s_my_set)
+    print(type(s_my_set))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
