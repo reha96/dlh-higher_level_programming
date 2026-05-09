b76ffba4cfc59ac3708a60b9bcb02aa91c62406b
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-09 15:54:12 +0200

    Create 9-student.py

---
+++ python-input_output/9-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Write a class Student that defines a student by:
+
+    Public instance attributes:
+    first_name
+    last_name
+    age
+    Instantiation with first_name, last_name and age: def __init__(self,
+    first_name, last_name, age):
+    Public method def to_json(self): that retrieves a dictionary
+    representation of a Student instance (same as 8-class_to_json.py)
+    You are not allowed to import any module
+
+"""
+
+
+class Student:
+    """
+    create Student class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """initiation method
+        Args:
+            first_name (_type_): _description_
+            last_name (_type_): _description_
+            age (_type_): _description_
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Return dictionary representation of Student instance"""
+        return self.__dict__
