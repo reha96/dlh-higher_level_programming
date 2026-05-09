247f0b0877ed9ffebf088ef2d52e14115d2f3236
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-09 10:03:30 +0200

    create 3, test, and push

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A module that writes a function that returns the JSON representation
+of an object (string)"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """A function that returns the JSON representation of an object
+    (string)
+
+    Args:
+        my_obj (_type_): _description_
+    """
+    return json.dumps(my_obj)
--- python-input_output/tests/0-main.py
+++ python-input_output/tests/0-main.py
@@ -1,94 +1,98 @@
 """tester file"""
+
 import os
 
-os.system('clear')
-print("--"*10)
-print("tester 0")
-print("--"*5 + "pycode" + "--"*5)
+os.system("clear")
+print("--" * 10)
+print("tester 0-read_file.py")
+print("--" * 5 + "pycode" + "--" * 5)
 os.system("pycodestyle 0-read_file.py")
-print("--"*5 + "pycode" + "--"*5)
+print("--" * 5 + "pycode" + "--" * 5)
 
-read_file = __import__('0-read_file').read_file
+read_file = __import__("0-read_file").read_file
 
 try:
     read_file("tests/my_file_0.txt")
 except Exception as err:
     print("[{}] {}".format(err.__class__.__name__, err))
 
-os.system('clear')
-print("--"*10)
-print("tester 1")
-print("--"*5 + "pycode" + "--"*5)
+os.system("clear")
+print("--" * 10)
+print("tester 1-write_file.py")
+print("--" * 5 + "pycode" + "--" * 5)
 os.system("pycodestyle 1-write_file.py")
-print("--"*5 + "pycode" + "--"*5)
+print("--" * 5 + "pycode" + "--" * 5)
 
-write_file = __import__('1-write_file').write_file
+write_file = __import__("1-write_file").write_file
 
 try:
     nb_characters = write_file(
-        "my_first_file.txt", "This School is so cool!\n")
+        "my_first_file.txt", "This School is so cool!\n"
+    )
     print(nb_characters)
 except Exception as err:
     print("[{}] {}".format(err.__class__.__name__, err))
 
-os.system('clear')
-print("--"*10)
-print("tester 1")
-print("--"*5 + "pycode" + "--"*5)
+os.system("clear")
+print("--" * 10)
+print("tester 2-append_write.py")
+print("--" * 5 + "pycode" + "--" * 5)
 os.system("pycodestyle 2-append_write.py")
-print("--"*5 + "pycode" + "--"*5)
+print("--" * 5 + "pycode" + "--" * 5)
 
-append_write = __import__('2-append_write').append_write
+append_write = __import__("2-append_write").append_write
 
 try:
     print("test: file does not exist")
     nb_characters_added = append_write(
-        "file_append.txt", "This School is so cool!\n")
+        "file_append.txt", "This School is so cool!\n"
+    )
     print(nb_characters_added)
 except Exception as err:
     print("[{}] {}".format(err.__class__.__name__, err))
 
 try:
     print("test: append existing")
-    os.system('rm my_first_file.txt')
+    os.system("rm my_first_file.txt")
     nb_characters = write_file(
-        "my_first_file.txt", "This School is so cool!\n")
+        "my_first_file.txt", "This School is so cool!\n"
+    )
     nb_characters_added = append_write(
-        "my_first_file.txt", "This School is so cool!\n")
+        "my_first_file.txt", "This School is so cool!\n"
+    )
     print(nb_characters_added)
 except Exception as err:
     print("[{}] {}".format(err.__class__.__name__, err))
 
-os.system('clear')
-print("--"*10)
-print("tester 1")
-print("--"*5 + "pycode" + "--"*5)
-os.system("pycodestyle 2-append_write.py")
-print("--"*5 + "pycode" + "--"*5)
+os.system("clear")
+os.system("rm my_first_file.txt")
+os.system("rm file_append.txt")
+print("--" * 10)
+print("tester 3-to_json_string.py")
+print("--" * 5 + "pycode" + "--" * 5)
+os.system("pycodestyle 3-to_json_string.py")
+print("--" * 5 + "pycode" + "--" * 5)
 
-to_json_string = __import__('3-to_json_string').to_json_string
+to_json_string = __import__("3-to_json_string").to_json_string
 
 my_list = [1, 2, 3]
 s_my_list = to_json_string(my_list)
 print(s_my_list)
 print(type(s_my_list))
 
-my_dict = { 
-    'id': 12,
-    'name': "John",
-    'places': [ "San Francisco", "Tokyo" ],
-    'is_active': True,
-    'info': {
-        'age': 36,
-        'average': 3.14
-    }
+my_dict = {
+    "id": 12,
+    "name": "John",
+    "places": ["San Francisco", "Tokyo"],
+    "is_active": True,
+    "info": {"age": 36, "average": 3.14},
 }
 s_my_dict = to_json_string(my_dict)
 print(s_my_dict)
 print(type(s_my_dict))
 
 try:
-    my_set = { 132, 3 }
+    my_set = {132, 3}
     s_my_set = to_json_string(my_set)
     print(s_my_set)
     print(type(s_my_set))
