0fdfaa2f1dd4c94e941422967852c17abd7c42d2
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-09 15:11:48 +0200

    Update 8-class_to_json.py

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -13,26 +13,5 @@ def class_to_json(obj):
     Args:
         obj (_type_): _description_
     """
-    
-    return obj
 
-class MyClass:
-    """ My class
-    """
-
-    def __init__(self, name):
-        self.name = name
-        self.number = 0
-
-    def __str__(self):
-        return "[MyClass] {} - {:d}".format(self.name, self.number)
-
-
-m = MyClass("John")
-m.number = 89
-# print(type(m))
-# print(m)
-
-mj = class_to_json(m)
-print(type(mj))
-# print(mj)
+    return obj.__dict__
