0e890e38fea70c8bde24d8be483aa0d0cf795049
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-09 17:14:52 +0200

    Update 10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -44,17 +44,5 @@ class Student:
                 for key in pop_keys:
                     out.pop(key)
             except:
-                pass            
+                pass
             return out
-
-
-student_1 = Student("John", "Doe", 23)
-student_2 = Student("Bob", "Dylan", 27)
-
-j_student_1 = student_1.to_json()
-j_student_2 = student_2.to_json(["first_name", "age"])
-j_student_3 = student_2.to_json(["middle_name", "age"])
-
-print(j_student_1)
-print(j_student_2)
-print(j_student_3)
