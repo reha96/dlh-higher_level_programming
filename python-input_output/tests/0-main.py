1399f00c556d7941908e9108b508202fd6c01431
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-09 10:14:19 +0200

    Update 0-main.py

--- python-input_output/tests/0-main.py
+++ python-input_output/tests/0-main.py
@@ -130,3 +130,36 @@ try:
     print(type(my_dict))
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
+
+os.system("clear")
+print("--" * 10)
+print("tester 5-save_to_json_file.py")
+print("--" * 5 + "pycode" + "--" * 5)
+os.system("pycodestyle 5-save_to_json_file.py")
+print("--" * 5 + "pycode" + "--" * 5)
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+filename = "my_list.json"
+my_list = [1, 2, 3]
+save_to_json_file(my_list, filename)
+
+filename = "my_dict.json"
+my_dict = { 
+    'id': 12,
+    'name': "John",
+    'places': [ "San Francisco", "Tokyo" ],
+    'is_active': True,
+    'info': {
+        'age': 36,
+        'average': 3.14
+    }
+}
+save_to_json_file(my_dict, filename)
+
+try:
+    filename = "my_set.json"
+    my_set = { 132, 3 }
+    save_to_json_file(my_set, filename)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
