4f94b38ba6ed775ce0007da4496f2c1a626bdf7c
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-09 10:08:37 +0200

    Update 0-main.py

--- python-input_output/tests/0-main.py
+++ python-input_output/tests/0-main.py
@@ -64,9 +64,9 @@ try:
 except Exception as err:
     print("[{}] {}".format(err.__class__.__name__, err))
 
-os.system("clear")
-os.system("rm my_first_file.txt")
 os.system("rm file_append.txt")
+os.system("rm my_first_file.txt")
+os.system("clear")
 print("--" * 10)
 print("tester 3-to_json_string.py")
 print("--" * 5 + "pycode" + "--" * 5)
@@ -96,5 +96,37 @@ try:
     s_my_set = to_json_string(my_set)
     print(s_my_set)
     print(type(s_my_set))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+os.system("clear")
+print("--" * 10)
+print("tester 4-from_json_string.py")
+print("--" * 5 + "pycode" + "--" * 5)
+os.system("pycodestyle 4-from_json_string.py")
+print("--" * 5 + "pycode" + "--" * 5)
+
+from_json_string = __import__("4-from_json_string").from_json_string
+
+s_my_list = "[1, 2, 3]"
+my_list = from_json_string(s_my_list)
+print(my_list)
+print(type(my_list))
+
+s_my_dict = """
+{"is_active": true, "info": {"age": 36, "average": 3.14}, 
+"id": 12, "name": "John", "places": ["San Francisco", "Tokyo"]}
+"""
+my_dict = from_json_string(s_my_dict)
+print(my_dict)
+print(type(my_dict))
+
+try:
+    s_my_dict = """
+    {"is_active": true, 12 }
+    """
+    my_dict = from_json_string(s_my_dict)
+    print(my_dict)
+    print(type(my_dict))
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
