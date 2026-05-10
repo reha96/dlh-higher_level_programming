90f3a7c6b971f54be36b6db4293c3a6e01200760
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-10 20:05:15 +0200

    Create 102-square.py

---
+++ python-classes/102-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""This module creates a class that defines a square without
+importing any modules."""
+
+
+class Square:
+    """Create a class called Square that defines a square."""
+
+    def __init__(self, size=0):
+        """Initializes the size of the Square, as a private
+        attribute.
+        1) size must be an integer, otherwise raise a TypeError
+        exception with the message size must be an integer.
+        2) if size is less than 0, raise a ValueError exception
+        with the message size must be >= 0
+
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Public instance method def area(self) returns the
+        current square area."""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
