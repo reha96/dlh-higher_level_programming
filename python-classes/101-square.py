68a8e07e15fa17bd9ad84dbe1d48cbe1a01c871d
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-10 19:57:22 +0200

    Create 101-square.py

---
+++ python-classes/101-square.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+"""This module creates a class that defines a square without
+importing any modules."""
+
+
+class Square:
+    """Create a class called Square that defines a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes the size of the Square, as a private
+        attribute.
+        1) size must be an integer, otherwise raise a TypeError
+        exception with the message size must be an integer.
+        2) if size is less than 0, raise a ValueError exception
+        with the message size must be >= 0
+
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def position(self):
+        """Public instance method def position(self) returns the
+        current position."""
+        return self.__position
+
+    @position.setter
+    def position(self, position):
+        if (
+            not isinstance(position, tuple)
+            or len(position) != 2
+            or not all(isinstance(p, int) and p >= 0 for p in position)
+        ):
+            raise TypeError(
+                "position must be a tuple of 2 positive integers"
+            )
+        else:
+            self.__position = position
+
+    def area(self):
+        """Public instance method def area(self) returns the
+        current square area."""
+        return self.__size**2
+
+    @property
+    def size(self):
+        """Public instance def size(self) returns the
+        current size."""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """Public instance def size(self, size) sets the
+        current size."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def my_print(self):
+        if self.size == 0:
+            print("")
+        if self.position[1] > 0:
+            for i in range(self.position[1]):
+                print("")
+        for j in range(self.size):
+            print(" " * self.__position[0] + "#" * self.__size)
+
+    def __str__(self):
+        if self.size == 0:
+            return ""
+        out = []
+        if self.position[1] > 0:
+            for i in range(self.position[1]):
+                out.append("")
+        for j in range(self.size):
+            out.append(" " * self.__position[0] + "#" * self.__size)
+            out.append("\n")
+        out = "".join(out)
+        return out[:-1]
