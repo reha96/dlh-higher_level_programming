75e3e02819464857d00d0bf13cd016c7f1f8bab4
reha96 <51120957+reha96@users.noreply.github.com>
2026-05-10 19:08:11 +0200

    Update 100-singly_linked_list.py

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -41,11 +41,10 @@ class SinglyLinkedList:
         if new_node.data <= self.__head.data:
             new_node.next_node = self.__head
             self.__head = new_node
-        
+            return
+
         current = self.__head
-        
         prev = None
-        
         while current is not None and new_node.data > current.data:
             prev = current
             current = current.next_node
@@ -62,20 +61,21 @@ class SinglyLinkedList:
         return "\n".join(values)
 
 
-sll = SinglyLinkedList()
-sll.sorted_insert(1)
-sll.sorted_insert(0)
-sll.sorted_insert(2)
-sll.sorted_insert(5)
-sll.sorted_insert(3)
-sll.sorted_insert(3)
-sll.sorted_insert(10)
-sll.sorted_insert(1)
-sll.sorted_insert(-4)
-sll.sorted_insert(-3)
-sll.sorted_insert(4)
-sll.sorted_insert(5)
-sll.sorted_insert(12)
-sll.sorted_insert(3)
-sll.sorted_insert(90)
-print(sll)
+if __name__ == "__main__":
+    sll = SinglyLinkedList()
+    sll.sorted_insert(1)
+    sll.sorted_insert(0)
+    sll.sorted_insert(2)
+    sll.sorted_insert(5)
+    sll.sorted_insert(3)
+    sll.sorted_insert(3)
+    sll.sorted_insert(10)
+    sll.sorted_insert(1)
+    sll.sorted_insert(-4)
+    sll.sorted_insert(-3)
+    sll.sorted_insert(4)
+    sll.sorted_insert(5)
+    sll.sorted_insert(12)
+    sll.sorted_insert(3)
+    sll.sorted_insert(90)
+    print(sll)
